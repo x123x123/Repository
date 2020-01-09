9c2d53d0debe93430a84d30874b2d9bbd5377788
Hanser0 <52728322+Hanser0@users.noreply.github.com>
2020-01-10 00:13:18 +0800

    n

---
+++ DataStructuresAndAlgorithm/BuyChicken/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyChicken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 100; a++)  //a公鸡数量
+                for (int b = 0; b <= 100; b++)  //b母鸡数量
+                    for (int c = 0; c <= 100; c++)  //c小鸡数量
+                        if ((a + b + c) == 100 && (a * 5 + b * 3 + c / 3) == 100 && c % 3 == 0)
+                            Console.WriteLine("公鸡{0}只,母鸡,{1}只，小鸡{2}\n", a, b, c);//
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
---
+++ DataStructuresAndAlgorithm/Calculateage/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using 计算年龄和姓氏.Properties
+
+namespace 计算年龄和姓氏
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var result = "";
+            var day = 0;
+            var now = DateTime.Now;
+            ListClass list = new ListClass();
+            foreach (var item in list.InitialPersonList())
+            {
+                day = day + Math.Abs(((TimeSpan)(now - item.Birthday)).Days);
+            }
+
+            var avgday = day / list.InitialPersonList().Count;
+            var avgYears = avgday / 365;
+            var avgDays = avgday - avgYears * 365;
+
+            result = "平均年龄：" + avgYears + "周岁" + "-" + avgDays + "天";
+            Console.WriteLine(result);
+            GetName(list.InitialPersonList());
+        }
+        public static void GetName(List<Person> persons)
+        {
+            var result1 = from i in persons
+                          where i.Name.Contains("王某")
+                          group i by i.Name.Substring(0, 2)
+                          into g
+                          select new { g.Key, sum = g.Count() };
+            var result2 = from i in persons
+                          where !i.Name.Contains("王某")
+                          group i by i.Name.Substring(0, 1)
+                          into g
+                          select new { g.Key, sum = g.Count() };
+            foreach (var x in result2)
+            {
+                Console.Write("姓氏：{0}\t{1}人\n", x.Key, x.sum);
+            }
+            foreach (var x in result1)
+            {
+                Console.Write("姓氏：{0}\t{1}人\n", x.Key, x.sum);
+            }
+        }
+    }
+}
